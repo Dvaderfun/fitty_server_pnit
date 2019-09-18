471dcbbcde29b49775ca99f98e6c4bf71f95c777
Daniil Dermenzhy <dvaderfun@gmail.com>
2019-09-18 12:13:43 +0300

    Initial commit

---
+++ src/main/java/com/pnitonpu/fittytestmy11/Fittytestmy11Application.java
@@ -0,0 +1,13 @@
+package com.pnitonpu.fittytestmy11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fittytestmy11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fittytestmy11Application.class, args);
+    }
+
+}
